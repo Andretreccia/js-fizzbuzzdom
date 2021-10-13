dcbdda454e4ec7015af03693c3120e0428f5e791
Andretreccia <andreabtreccia@gmail.com>
2021-10-13 14:45:46 +0200

    Avanzamento struttura del programma
    verificate che le condizioni funzionino tramite console.log

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,23 @@
 per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
 
 //Creare un ciclo nel quale vengano visualizzati fino a 100 elementi numerati
+for (let i = 1; i <= 100; i++) {
 
 //all interno del ciclo inserire le condizioni con le quali individuare i multipli di 3 e 5
-
     //condizione nella quale i è divisibile per 3 con resto 0 (i con % =  0 funzionerà??)
-
+    if (i % 3 == 0) {
+        console.log('multiploditre');
+    }
     //condizione nella quale i è divisibile per 5 con resto 0 (i con % =  0 funzionerà??)
+    else if (i % 5 == 0) {
+        console.log('multiplodicinque');
+    }
+    //se non è un multiplo delle condizioni precedenti allora visualizza il numero
+    else {
+        console.log(i)
+    }
+}
+
+
 
 //far creare i deversi elementi in base alle condizioni impostate
