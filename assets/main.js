245b958bf17103e1388871e6e5b8e3a38a9aa1c9
Andretreccia <andreabtreccia@gmail.com>
2021-10-13 14:25:00 +0200

    Dichiarati gli step del programma

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,12 @@
 /* Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
 per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
+
+//Creare un ciclo nel quale vengano visualizzati fino a 100 elementi numerati
+
+//all interno del ciclo inserire le condizioni con le quali individuare i multipli di 3 e 5
+
+    //condizione nella quale i è divisibile per 3 con resto 0 (i con % =  0 funzionerà??)
+
+    //condizione nella quale i è divisibile per 5 con resto 0 (i con % =  0 funzionerà??)
+
+//far creare i deversi elementi in base alle condizioni impostate
